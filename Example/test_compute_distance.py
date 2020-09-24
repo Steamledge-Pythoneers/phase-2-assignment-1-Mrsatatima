15634c2cc417c4aeb10e1e731bf34752992088f4
Al-amin Ahmad Abbas <aminuozil@gmail.com>
2020-09-24 11:22:09 +0100

    Editing 14 to 4 to pass test

--- Example/test_compute_distance.py
+++ Example/test_compute_distance.py
@@ -1,14 +1,14 @@
 from compute_distance import distance_to_destination
 
 def test_distance_to_destination_4():
-    assert(distance_to_destination(22, 26) == 14)
+    assert(distance_to_destination(22, 26) == 4)
 
 def test_distance_to_destination_0():
     assert(distance_to_destination(253, 253) == 0)
 
 def test_distance_to_destination_0_negative():
     assert(distance_to_destination(83, 64) == 0)
-    
+
 def test_distance_to_destination_1():
     assert(distance_to_destination(9, 10) == 1)
 
