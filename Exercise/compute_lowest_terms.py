ea184bf34dc2dc935d97f1cb3c83a18f917d9273
Al-amin Ahmad Abbas <aminuozil@gmail.com>
2020-09-24 16:38:25 +0100

    changing values as instructed by Drako in  test_compute_lowest_terms

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -3,11 +3,12 @@ from fractions import Fraction
 def lowest_terms(x):
 	try:
 		y = Fraction(x)
+		return str(y.numerator)+"/"+str(y.denominator)
 	except:
 		a = int(x.split("/")[0])
 		b =int(x.split("/")[1])
 		y= Fraction(a, b)
-	return str(y.numerator)+"/"+str(y.denominator)
+		return str(y.numerator)+"/"+str(y.denominator)
 
 	#x = x.split("/")
 	#y= (Fraction(int(x[0]),int(x[1])))
