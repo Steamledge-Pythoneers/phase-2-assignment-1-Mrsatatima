cbeffaf78eb31e40ca48947fc88d1f04862e814f
Al-amin Ahmad Abbas <aminuozil@gmail.com>
2020-09-24 15:02:22 +0100

    Accessing numerator and denominator of Fraction()

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,5 +1,15 @@
 ## TODO: complete the function "lowest_terms" below
 from fractions import Fraction
 def lowest_terms(x):
-	return Fraction(x)
-print(lowest_terms("4/7"))
+	y = Fraction(x)
+	return str(y.numerator)+"/"+str(y.denominator)
+
+	#x = x.split("/")
+	#y= (Fraction(int(x[0]),int(x[1])))
+	#print (lowest_terms("7/49"))
+
+#
+#x= input("enter Number: ")
+#y=Fraction(x)
+#y = y.split("/")
+#print(y)
