6bbf21e4563c44fe5083536dbc58450f318b96ab
Al-amin Ahmad Abbas <aminuozil@gmail.com>
2020-09-24 20:45:14 +0100

    adding if statement to test O numerator

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,17 +1,19 @@
 ## TODO: complete the function "lowest_terms" below
 from fractions import Fraction
 def lowest_terms(x):
-	try:
-		y = Fraction(x)
-		return str(y.numerator)+"/"+str(y.denominator)
-	except(ValueError):
-		a = int(x.split("/")[0])
-		b =int(x.split("/")[1])
-		y= Fraction(a, b)
-		return str(y.numerator)+"/"+str(y.denominator)
-		if y.numerator == 0:
-			return "0"
-	except ZeroDivisionError:
-		y= "Undefined"
-		return y
-	
+	if x[0] == "0":
+		return x[0]
+	else:
+		try:
+			y = Fraction(x)
+			return str(y.numerator)+"/"+str(y.denominator)
+		except(ValueError):
+			a = int(x.split("/")[0])
+			b =int(x.split("/")[1])
+			y= Fraction(a, b)
+			return str(y.numerator)+"/"+str(y.denominator)
+			if y.numerator == 0:
+				return "0"
+		except ZeroDivisionError:
+			y= "Undefined"
+			return y
